c7d339c67b3073320ceac138863abecf54bde93c
GardenLee <khjvvv7@gmail.com>
2020-05-09 20:21:04 +0900

    KakaoWinter4

---
+++ KakaoWinter2019/KakaoWinter4.java
@@ -0,0 +1,30 @@
+//package programmers.KakaoWinter2019;
+//
+//import java.util.HashMap;
+//import java.util.Map;
+//
+//public class KakaoWinter4 {
+//    public static void main(String[] args) {
+//        KakaoWinter4 kaka = new KakaoWinter4();
+//        long k = 10;
+//        long[] room_number = {1,3,4,1,3,1};
+//
+//        long[] answer = kaka.solution(k, room_number);
+//        System.out.println(answer);
+//    }
+//
+//    public long[] solution(long k, long[] room_number) {
+//        long[] answer = {};
+//        HashMap<Long, Long> roomMap = new HashMap<>();
+//
+//        for (int i=0; i<room_number.length; i++) {
+//            if (roomMap.containsKey(room_number[i])) {
+//
+//            } else {
+//                roomMap.put(room_number[i], r);
+//            }
+//        }
+//
+//        return answer;
+//    }
+//}
